e6eff294878b5b308e4069add543f81a321014ba
akash12patel12 <akash12patel12@gmail.com>
2022-11-09 19:42:24 +0530

    delete/add/edit implemented with sequelize

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require("sequelize");
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node_complete',
-    password: '12345678'
+const sequelize = new Sequelize("node_complete", "root", "12345678", {
+  dialect: "mysql",
+  host: "localhost",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
